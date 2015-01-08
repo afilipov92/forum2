3149a7f26b78aab0318d26ff2cd43642a8fc0d36
Andrei-Drozd <fiver80@gmail.com>
2015-01-08 12:38:14 +0300

    Added close tags to logout.php

--- logout.php
+++ logout.php
@@ -4,8 +4,8 @@
   if (isset($_SESSION['userName'])){
       session_destroy();
       echo "<div class='main'>You have been logged out. Please " .
-      "<a href='login.php'>click here</a> to proceed for login screen.";
+      "<a href='login.php'>click here</a> to proceed for login screen.</div>";
   } else {
-      echo "<div class='main'><br>" .
-          "You cannot log out because you are not logged in";
+      echo "<div class='main'><br></div>" .
+          " cannot log out because you are not logged in";
   }
